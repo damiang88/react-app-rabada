61300dfa37b3bdee7a6e7020c4d19f28284ee17d
damiang88 <73801650+damiang88@users.noreply.github.com>
2022-06-06 21:50:51 -0300

    NavBar v1

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+export default function NavBar(){
+
+    return (
+        <nav class="sb-topnav navbar navbar-expand navbar-light bg-dark shadow">
+                <a class="navbar-brand ps-3 text-light" href="./">  
+                    Home
+                </a>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active ml-3"> 
+                        <a class="nav-link text-light" href="./" >
+                            Productos
+                        </a> 
+                    </li>
+                    <li class="nav-item active ml-3"> 
+                        <a class="nav-link text-light" href="./" >
+                        Ofertas
+                        </a> 
+                    </li>
+                </ul>
+        </nav>
+
+);
+}
